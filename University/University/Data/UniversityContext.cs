e3b621291d30dae83f29f4ce950d186460b19b5b
ATh3tr <aeter520@gmail.com>
2026-03-09 19:58:22 +0300

    Add assignment2 project

---
+++ University/University/Data/UniversityContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using University.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+namespace University.Data;
+
+public class UniversityContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Course> Courses { get; set; } = null!;
+    public DbSet<Comment> Comments { get; set; } = null!;
+    public DbSet<Grade> Grades { get; set; } = null!;
+    public DbSet<Syllabus> Syllabuses { get; set; } = null!;
+    public DbSet<Assignment> Assignments { get; set; } = null!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure relationship with ON DELETE NO ACTION
+        modelBuilder.Entity<Course>()
+            .HasOne(c => c.Teacher)
+            .WithMany(u => u.Courses)
+            .HasForeignKey(c => c.TeacherId)
+            .OnDelete(DeleteBehavior.Restrict); // Maps to NO ACTION in SQL Server
+        // Configure relationship with ON DELETE NO ACTION
+        modelBuilder.Entity<Grade>()
+            .HasOne(c => c.Student)
+            .WithMany(u => u.Grades)
+            .HasForeignKey(c => c.StudentId)
+            .OnDelete(DeleteBehavior.Restrict); // Maps to NO ACTION in SQL Server
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-0TTJB4N\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=True;Application Name=""SQL Server Management Studio"";Command Timeout=0");
+    }
+
+}
---
+++ University/University/Models/Syllabus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models
+{
+    public class Syllabus
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        // not sure if this should be one-to-one instead
+        public Course? Course { get; set; } 
+        public Syllabus() { }
+        public Syllabus(String content)
+        {
+            Content = content;
+        }
+    }
+}
